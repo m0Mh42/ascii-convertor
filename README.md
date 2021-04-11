64f5a4742cbf88f169e5315a1e71874a6afe1b78
m0Mh42 <mohammadmahdi13840104@gmail.com>
2021-04-11 16:40:45 +0430

    Initial Release

---
+++ README.md
@@ -0,0 +1,5 @@
+# Ascii Convertor
+
+This is my C app to convert any text I want to decimal, hexadecimal or octal format.
+A pretty simple code, it's not gonna do much :).
+
---
+++ main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char word[BUFSIZ], mode;
+    int i, len;
+
+    printf("Text: ");
+    fgets(word, BUFSIZ, stdin);
+    len = strlen(word);
+
+    printf("Convert to (d: decimal, x: hex, o: octal): ");
+    scanf("%c", &mode);
+
+    if (word[len-1] == '\n'){
+        word[--len] = '\0';
+    }
+
+    printf("Calculating...\n");
+
+    if (mode == 'd'){
+        // DECIMAL
+        for (i = 0; i < len; i++){
+            printf("%d ", word[i]);
+        }
+    } else if (mode == 'x'){
+        //HEX
+        for (i = 0; i < len; i++){
+            printf("%02x ", word[i]);
+        }
+    } else if (mode == 'o'){
+        //OCT
+        for (i = 0; i < len; i++){
+            printf("%o ", word[i]);
+        }
+    } else {
+        printf("Mode must be decimal or hex or octal.");
+    }
+
+    printf("\n");
+    return 0;
+}
